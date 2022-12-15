5ab957301ef1c2285856103f55f81ec884860dea
Антон Деревягин <antonderevagin@MacBook-Air-Anton.local>
2022-12-16 02:50:01 +0300

    Zadacha3

---
+++ Zadacha4/Program.cs
@@ -0,0 +1,34 @@
+﻿int size = 10;
+int[] array = new int[size];
+FillArrayRandomNumbers(array);
+
+
+int max = array[0];
+int min = array[0];
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] > max)
+    {
+        max = numbers[i];
+    }
+    else if (numbers[i] < min)
+    {
+        min = numbers[i];
+    }
+}
+
+Console.WriteLine($"Минимальное число: {min}");
+Console.WriteLine($"Максимальное число: {max}");
+Console.WriteLine($"Разница между максимальным и минимальным числами: {max-min}");
+
+
+void FillArrayRandomNumbers(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 555);
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
