8133f565a472e64c7b8e4ac9674abf57fa06b5a2
Антон Деревягин <antonderevagin@MacBook-Air-Anton.local>
2022-12-17 14:16:38 +0300

    Zadacha1

---
+++ Zadacha1/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+int count = 0;
+FillArrayRandomNumbers(numbers);
+
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] % 2 == 0)
+    {
+        count++;
+    }
+    
+}
+Console.WriteLine($"количество чётных чисел в массиве -> {count} ");
+
+void FillArrayRandomNumbers(int[] array)// Прокручивает массив заполняя его числами и выводит на экран
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(100,1000);
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
